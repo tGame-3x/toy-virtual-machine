007b574ad3d08127f7bbd0c96c402a6f137f8e17
tGame-3x <68759664+tGame-3x@users.noreply.github.com>
2022-03-28 17:43:49 +0800

    init: 初始化

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int n = 10000;
+    std::cout << "Test for Random Array , size = " << n << ", random range [0, " << n << "]" << std::endl;
+    return 0;
+}
