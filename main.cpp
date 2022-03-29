309c3aabdca35292a839fbb0491a71cabd2edad8
tGame-3x <68759664+tGame-3x@users.noreply.github.com>
2022-03-29 09:14:39 +0800

    init: init

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     int n = 10000;
-    std::cout << "Test for Random Array , size = " << n << ", random range [0, " << n << "]" << std::endl;
+    std::cout << "Random Array , size = " << n << ", random range [0, " << n << "]" << std::endl;
     return 0;
 }
